e2597eef95ef59abc4fa871021e01223ffa88df4
Amir Ziarati <towardsucceess@gmail.com>
2019-07-18 22:15:31 +0200

    delete stetho

--- RickAndMorty/app/src/main/java/com/gustlogix/rickandmorty/MyApplication.java
+++ RickAndMorty/app/src/main/java/com/gustlogix/rickandmorty/MyApplication.java
@@ -2,12 +2,10 @@ package com.gustlogix.rickandmorty;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
-
 public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         SimpleServiceLocator.init(this);
-        Stetho.initializeWithDefaults(this);
+//        Stetho.initializeWithDefaults(this);
     }
 }
